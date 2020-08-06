000d4e663ef6776a9c6b5b30498aff0dc3e3125d
Nick <nik.silyaev@gmail.com>
2020-08-05 22:40:45 -0700

    db

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-octagonal-81946',
-        user: 'postgres',
-        password: 'root',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
